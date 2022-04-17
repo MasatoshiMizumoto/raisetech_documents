55f6d556c05781783d9930f5939d2d7738762418
MasatoshiMizumoto <51846213+MasatoshiMizumoto@users.noreply.github.com>
2022-04-17 21:33:50 +0900

    add samples

---
+++ aws/samples/serverspec/sample_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+listen_port = 8080
+
+describe package('nginx') do
+  it { should be_installed }
+end
+
+describe port(listen_port) do
+  it { should be_listening }
+end
+
+describe command('curl http://127.0.0.1:#{listen_port}/_plugin/head/ -o /dev/null -w "%{http_code}\n" -s') do
+  its(:stdout) { should match /^200$/ }
+end
