dd4e6400d9ef253a1a1a45342820f88336159293
MasatoshiMizumoto <51846213+MasatoshiMizumoto@users.noreply.github.com>
2024-12-01 21:55:53 +0900

    fix: nvm&rvm構成に切り戻し

--- aws/scripts/vscode_server.sh
+++ aws/scripts/vscode_server.sh
@@ -6,19 +6,23 @@ RUBY_VERSION=3.2.3
 
 set -x
 
+sudo dnf update -y
+sudo dnf install -y git
+
 # change to ec2-user
 sudo su - ec2-user
 
-# Install nodejs
+
+# Install nvm and node
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
 source ~/.bashrc
 nvm install node
 
-# Install rvm
+# Install rvm and ruby
 curl -sSL https://rvm.io/mpapis.asc | gpg2 --import -
 curl -sSL https://rvm.io/pkuczynski.asc | gpg2 --import -
 curl -sSL https://get.rvm.io | bash -s stable
-# add source ~/.rvm/scripts/rvm to ~/.bashrc
+## add source ~/.rvm/scripts/rvm to ~/.bashrc
 echo 'source ~/.rvm/scripts/rvm' >> ~/.bashrc
 source ~/.bashrc
 rvm install $RUBY_VERSION
