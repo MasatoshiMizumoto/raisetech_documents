f4f16eea639d46e33cd6c0738bc830031653b3d1
MasatoshiMizumoto <51846213+MasatoshiMizumoto@users.noreply.github.com>
2024-12-01 21:21:03 +0900

    add: step1スクリプト追加

---
+++ aws/scripts/al2023_setup_step1.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+set -x
+
+# change to ec2-user
+sudo su - ec2-user
+
+# install asdf
+sudo dnf -y install git
+git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.14.1
+echo '. $HOME/.asdf/asdf.sh' >> ~/.bashrc
+
+# shell reload
+source ~/.bash_profile
+
+## plugin
+asdf plugin add nodejs
+asdf plugin update nodejs
+asdf plugin add ruby
+asdf plugin update ruby
+
+echo """
+
+サーバーを再起動します。
+再起動後、Step2のスクリプトを実行してください。
+
+## English
+
+This server will be rebooted.
+After reboot, please run the script for Step2.
+
+"""
+
+sleep 10
